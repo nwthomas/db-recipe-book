b1fd599dada9628d4c4ca0d289c5ec564e6df49a
Nathan Thomas <nwthomas@me.com>
2019-02-20 19:16:21 -0800

    Created .post() API route in the server

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ server.get("/api/dishes", async (req, res) => {
 
 server.get("/api/dishes/:id", async (req, res) => {
   try {
-    const dishes = await db("dishes").where({ id: req.params.id });
-    if (dishes) {
-      res.status(200).json(dishes);
+    const dish = await db("dishes").where({ id: req.params.id });
+    if (dish) {
+      res.status(200).json(dish);
     } else {
       res
         .status(404)
@@ -52,7 +52,27 @@ server.get("/api/dishes/:id", async (req, res) => {
 });
 
 server.post("/api/dishes", async (req, res) => {
-
+  if (!req.body) {
+    return res
+      .status(400)
+      .json("Please include a name for the dish and try again.");
+  }
+  try {
+    const dish = await db("dishes").insert({ name: req.body });
+    if (dish) {
+      res
+        .status(200)
+        .json({ message: "The dish was created successfully.", dish });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The dish could not be created at this time." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error creating the dish in the database."
+    });
+  }
 });
 
 module.exports = server;
