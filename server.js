e951b5d41db149cc6c9ba9c655f40de9a2256fdd
Nathan Thomas <nwthomas@me.com>
2019-02-20 18:44:05 -0800

    Create .get() all and by id dishes API routes

--- server.js
+++ server.js
@@ -1,12 +1,58 @@
 const express = require("express");
 const knex = require("knex");
 const knexConfig = require("./knexfile.js");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
 const db = knex(knexConfig.development);
 
 server.get("/", (req, res) => {
-  res.eventNames("Working!");
+  res.send("Working!");
+});
+
+// ================================================== Dishces
+server.get("/api/dishes", async (req, res) => {
+  try {
+    const dishes = await db("dishes");
+    if (dishes) {
+      res.status(200).json(dishes);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find any dishes in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the dishes.",
+      error
+    });
+  }
+});
+
+server.get("/api/dishes/:id", async (req, res) => {
+  try {
+    const dishes = await db("dishes").where({ id: req.params.id });
+    if (dishes) {
+      res.status(200).json(dishes);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find any dishes in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the dishes.",
+      error
+    });
+  }
+});
+
+server.post("/api/dishes", async (req, res) => {
+
 });
 
 module.exports = server;
